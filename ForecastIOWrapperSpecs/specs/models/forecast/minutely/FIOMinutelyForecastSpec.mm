61903b08e70422a7e3450bf8cb71a324b0a7c056
Kevin Jenkins <kevinj04@gmail.com>
2013-06-22 15:47:06 -0400

    add spec for MinutelyForecast

--- ForecastIOWrapperSpecs/specs/models/forecast/minutely/FIOMinutelyForecastSpec.mm
+++ ForecastIOWrapperSpecs/specs/models/forecast/minutely/FIOMinutelyForecastSpec.mm
@@ -1,4 +1,5 @@
 #import "FIOMinutelyForecast.h"
+#import "FIOMinuteDataPoint.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
@@ -6,10 +7,24 @@ using namespace Cedar::Doubles;
 SPEC_BEGIN(FIOMinutelyForecastSpec)
 
 describe(@"FIOMinutelyForecast", ^{
-    __block FIOMinutelyForecast *model;
+    __block FIOMinutelyForecast *forecast;
+    __block NSDictionary *dictionary;
 
-    beforeEach(^{
-
+    describe(@"updateDataPointsWithArray:", ^{
+        subjectAction(^{
+            forecast = [FIOMinutelyForecast forecastWithDictionary:dictionary];
+        });
+        describe(@"with a dictionary with a data points entry", ^{
+            beforeEach(^{
+                dictionary = @{FIOForecastDataKey:@[@{},@{}]};
+            });
+            it(@"should create two entries", ^{
+                forecast.dataPoints.count should equal([dictionary[FIOForecastDataKey] count]);
+            });
+            it(@"should create entries of type FIOMinuteDataPoint", ^{
+                [forecast.dataPoints[0] isKindOfClass:[FIOMinuteDataPoint class]] should be_truthy;
+            });
+        });
     });
 });
 
